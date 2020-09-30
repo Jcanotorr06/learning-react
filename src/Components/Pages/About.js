0278342acc3fed1976caa1fed0b10b190cfee03b
Jcanotorr06 <jcano062001@gmail.com>
2020-09-29 22:12:48 -0500

    created todo app

---
+++ src/Components/Pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        //React.Fragment is a fake bracket to avoid using divs :)
+        <React.Fragment> 
+            <h1>About</h1>
+            <p>This is a todo list app v1.0.0. It is part of a react crash course</p>
+        </React.Fragment>
+    )
+}
